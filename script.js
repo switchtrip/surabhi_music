9b7a515a779227f6f6121b7870fe29237c970ba7
switchtrip <aswitchtrip@gmail.com>
2024-03-12 14:38:46 +0530

    Add music and nav bar

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
 const eq = document.getElementById("eq");
-function animate(event){
-    eq.classList.toggle("active");
+const nextbtn = document.getElementById("next-btn");
+const prevbtn = document.getElementById("prev-btn");
+let audio = new Audio();
+var audFiles = ["Audio/test.wav", "Audio/barsega.wav", "Audio/AJT.wav"]
+// audio.src = "Audio/AJT.wav"
+// audio.loop = true;
+var i=0;
+audio.src = audFiles[i];
+
+function prevAudFile(){
+    if (i === 0) {
+        i = audFiles.length - 1;
+    } else {
+        i--;
+    }
+    // console.log(i);
+    // console.log(audio.src);
+    audio.src = audFiles[i];
+    audio.play();
 }
-eq.addEventListener("click", animate);
+function nextAudFile(){
+    if (i === audFiles.length - 1) {
+        i = 0;
+    } else {
+        i++;
+    }
+    audio.src = audFiles[i];
+    // console.log(i);
+    // console.log(audio.src);
+    audio.play();
+}
+function toggleAudio(){
+    if(audio.paused){
+        audio.play();
+    }
+    else{
+        audio.pause();
+    }
+}
+// eq.addEventListener("click", animate);
+eq.addEventListener("click", toggleAudio);
+
+audio.addEventListener('playing', animate, false)
+audio.addEventListener('pause', stopAnimate, false)
+audio.addEventListener('ended', nextAudFile, false)
+
+nextbtn.addEventListener("click", nextAudFile)
+prevbtn.addEventListener("click", prevAudFile)
+// Equalizer bars
+function animate(event){
+    console.log("Start animating");
+    eq.classList.add("active");
+}
+function stopAnimate(event){
+    console.log("Stop animating");
+    eq.classList.remove("active");
+}
