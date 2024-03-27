193a085fd98b5c0ed0a4ebc523a0d40298829d4f
switchtrip <aswitchtrip@gmail.com>
2024-03-27 23:37:53 +0530

    Close nav bar on mobile when focus out

--- nav.js
+++ nav.js
@@ -3,6 +3,20 @@ const navToggle = document.querySelector('.mobile-nav-toggle');
 
 navToggle.addEventListener("click", ()=>{
     const visibility = primaryNav.getAttribute("data-visible");
+    console.log("Toggle area clicked ");
+    console.log(visibility);
+    toggleNavArea(visibility);
+})
+// Toggle nav area on mobile view when clicked out
+document.addEventListener("click", (e)=>{
+    // const visibility = primaryNav.getAttribute("data-visible");
+    const outArea = !navToggle.contains(e.target) && !primaryNav.contains(e.target)
+    const visibility = primaryNav.getAttribute("data-visible");
+    console.log("Out area clicked", visibility==="true" && outArea);
+    if (visibility==="true" && outArea) toggleNavArea(visibility);
+})
+
+function toggleNavArea(visibility){
     if (visibility==="false"){
         primaryNav.setAttribute("data-visible", true);
         navToggle.setAttribute("aria-expanded", true);
@@ -11,4 +25,5 @@ navToggle.addEventListener("click", ()=>{
         primaryNav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
     }
-})
+
+}
